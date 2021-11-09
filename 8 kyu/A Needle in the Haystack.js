ed735eebf2596333da5f692dc79e40b77ddd959f
IvanFestina <ivanfestina@gmail.com>
2021-11-09 23:20:47 +0300

    in progress...

--- 8 kyu/A Needle in the Haystack.js
+++ 8 kyu/A Needle in the Haystack.js
@@ -1,4 +1,4 @@
-Can you find the needle in the haystack?
+/*Can you find the needle in the haystack?
 
 Write a function findNeedle() that takes an array full of junk but containing one "needle"
 
@@ -7,7 +7,7 @@ After your function finds the needle it should return a message (as a string) th
 "found the needle at position " plus the index it found the needle, so:
 
 findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
-should return "found the needle at position 5"
+should return "found the needle at position 5"*/
 
 
 
